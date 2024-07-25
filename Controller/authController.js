6f287bf20733fe4bcb14c5acda86d5de5f864aeb
Your Name <you@example.com>
2024-07-25 13:05:00 +0530

    user auth

--- Controller/authController.js
+++ Controller/authController.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken")
 const { checkEmpty } = require("../uitils/checkEmpty")
 const Admin = require("../models/Admin")
 const SendEmail = require("../uitils/email")
+const User = require("../models/User")
 
 exports.registerAdmin = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
@@ -57,7 +58,7 @@ exports.loginAdmin = asyncHandler(async (req, res) => {
     }
     const otp = Math.floor(10000 + Math.random() * 9000000)
 
-    await Admin.findByIdAndUpdate(result._id,{otp})
+    await Admin.findByIdAndUpdate(result._id, { otp })
     await SendEmail({
         to: email,
         subject: "Login OTP",
@@ -69,8 +70,8 @@ exports.loginAdmin = asyncHandler(async (req, res) => {
 
 })
 
-exports.verifyOTP = asyncHandler(async(req,res)=>{
-    const {otp,email}= req.body
+exports.verifyOTP = asyncHandler(async (req, res) => {
+    const { otp, email } = req.body
     const { isError, error } = checkEmpty({ email, otp })
     if (isError) {
         return res.status(401).json({ message: "All Fields Required" })
@@ -79,30 +80,86 @@ exports.verifyOTP = asyncHandler(async(req,res)=>{
         return res.status(401).json({ message: "Invaid Email" })
     }
     const result = await Admin.findOne({ email })
-    
+
     if (!result) {
         return res.status(401).json({
             message: process.env.NODE_ENV === "development" ?
-            "Invalid Password" : "Invalid Credintials"
+                "Invalid Password" : "Invalid Credintials"
         })
     }
-    if(otp !== result.otp){
+    if (otp !== result.otp) {
         return res.status(401).json({ message: "Invaid OTP" })
-      
+
     }
-    const token = jwt.sign({userId : result._id},process.env.JWT_KEY,{expiresIn:"1d"})
+    const token = jwt.sign({ userId: result._id }, process.env.JWT_KEY, { expiresIn: "1d" })
     // JWT
-    res.cookie("admin",token,{
+    res.cookie("admin", token, {
         maxAge: 864000000,
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "production"
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production"
     })
     //  cookie 
     res.json({
-       message:"OTP Verify Success",result:{
-        _id:result._id,
-        name:result.name,
-        email:result.email,
-       } 
+        message: "OTP Verify Success", result: {
+            _id: result._id,
+            name: result.name,
+            email: result.email,
+        }
     })
+})
+
+exports.logout = asyncHandler(async (rea, res) => {
+
+})
+
+exports.regiterUser = asyncHandler(async (rea, res) => {
+    const { name, mobile, email, password, cpassword } = req.body
+    const { errer, isError } = checkEmpty({
+        name, mobile, email, password, cpassword
+    })
+    if (isError) {
+        return res.status(400).json({ message: "All fields required".error })
+    }
+    if (!validator.isEmail(email)) { return res.status(400).json({ message: "Invalid email" }) }
+    if (!validator.isMobilePhone(mobile, "en-IN")) { return res.status(400).json({ message: "Invalid mobile" }) }
+    if (!validator.isStrongPassword(password)) { return res.status(400).json({ message: "Invalid password" }) }
+    if (!validator.isStrongPassword(cpassword)) { return res.status(400).json({ message: "Invalid cpassword" }) }
+    if (password !== cpassword) { return res.status(400).json({ message: "password do match" }) }
+    const hash = await bcrypt.hash(password, 10)
+    await User.create({ name, mobile, email, password: hash })
+    res.status(200).json({ message: "user Register Success" })
+})
+
+exports.loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const { isError, error } = checkEmpty({ email, password })
+
+    if (isError) { return res.status(400).json({ message: "All Fields Required" }) }
+
+    const result = await User.findOne({ email })
+    if (!result) {
+        return res.status(401).json({ message: "Email Not Found" })
+    }
+    const verify = await bcrypt.compare(password, result.password)
+    if (!verify) {
+        return res.status(401).json({ message: "Password Do Not Match" })
+    }
+
+    const token = jwt.sign({ userId: result._id }, process.env.JWT_KEY, { expiresIn: "180d" })
+
+    res.cookie("user", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        expiresIn: 1000 * 60 * 60 * 24 * 180
+    })
+
+
+    res.json({ message: "User Rgister Success" })
+
+})
+
+exports.logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie("User")
+    res.json({ message: "User Logout Success" })
 })
--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,8 +1,12 @@
-const { registerAdmin, loginAdmin,  verifyOTP,  } = require("../Controller/authController");
+
+const { registerAdmin, loginAdmin,  verifyOTP, loginUser, regiterUser, logoutUser,  } = require("../Controller/authController");
 const router = require("express").Router();
 
 router.post("/register-admin", registerAdmin)
       .post("/login-admin", loginAdmin)
       .post("/verify-otp", verifyOTP)
-
+// user
+.post("/login-mobile-user",loginUser)
+.post("/register-mobile-user",regiterUser)
+.post("/logout-mobile-user",logoutUser)
 module.exports = router;
